61dc23f0ccb1407af7df164d71a9ef747e5278cc
Jolene Kearse <pharaohnutz@gmail.com>
2022-09-16 15:57:42 -0400

    worked on Header with styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,40 @@
 import React, { useState } from "react";
-import SunSVG from "./images/icon-sun.svg";
+import Header from "./Header"
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [newItem, setNewItem] = useState("");
+  const [items, setItems] = useState("");
+
+  const setAndSaveItems = (newItem) => {
+    setItems(newItem);
+    localStorage.setItem("todolist", JSON.stringify(newItem));
+  };
+
+  const addItem = (item) => {
+    const id = items.length ? items[items.length - 1].id + 1 : 1;
+    const listItems = [...items];
+    setAndSaveItems(listItems);
+  }
 
   return (
     <React.Fragment>
-      <div id="app"></div>
+      <div id="app">
+        <Header
+          newItem={newItem}
+          setNewItem={setNewItem}
+        />
+      </div>
 
-      <header>
+      {/* <header>
         <div className="container">
           <h1>Todo</h1>
           <button id="toggle" title="Change light/dark mode">
             <img src={SunSVG} alt="" />
           </button>
         </div>
-      </header>
+      </header> */}
       <div className="container">
-        <form id="newTodo">
-          <button className="checkbox"></button>
-          <input
-            type="text"
-            name="newItem"
-            id="newItem"
-            placeholder="Create a new todo..."
-            required
-          />
-        </form>
+
 
         <div id="todoList">
           <div id="listItems"></div>
---
+++ client/src/Header.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import SunSVG from "./images/icon-sun.svg";
+import "./Header.css"
+
+const Header = (newItem, setNewItem) => {
+
+    return (
+        <header>
+            <div className="container align">
+                <div className="flex-spread">
+                    <h1>Todo</h1>
+                    <button
+                        id="toggle"
+                        title="Change light/dark mode"
+                        aria-label="Add Item">
+                        <img src={SunSVG} alt=""></img>
+                    </button>
+                </div>
+                <form id="newTodo">
+                    <div className="check-list">
+                        <button
+                            className="checkbox">
+                        </button>
+                        <input
+                            type="text"
+                            name="newItem"
+                            id="newItem"
+                            placeholder="Create a new todo..."
+                            required
+                        />
+                    </div>
+                </form>
+            </div>
+        </header>
+    )
+}
+
+export default Header
