29d2fa7c9733d7aaa7ea1e9ce0d9e7fbc530b2bf
Jolene Kearse <pharaohnutz@gmail.com>
2022-09-18 12:41:40 -0400

    updated App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,70 +18,49 @@ function App() {
 
   return (
     <React.Fragment>
-      <div className="app">
-        <header>
-          <div className="container">
-            <h1>Todo</h1>
-            <button className="toggle" title="Change light/dark mode">
-              <img src={SunSVG} alt="" />
-            </button>
-          </div>
-        </header>
-        <div className="container">
-          <form className="newTodo">
-            <div className="form-control">
-              <button className="checkbox"></button>
-              <input
-                type="text"
-                name="newItem"
-                className="newItem"
-                placeholder="Create a new todo..."
-                required
-              />
-            </div>
-          </form>
+      <Header />
 
-          <div className="todoList">
-            <div className="listItems"></div>
-            <div className="todoControls">
-              <div className="control-info">
-                <p>
-                  <span className="numberOfItems">5</span> items left
-                </p>
-                <button>Clear Completed</button>
-              </div>
-              <div className="items-control-group">
-                <button>All</button>
-                <button>Active</button>
-                <button>Completed</button>
-              </div>
-            </div>
-          </div>
 
-          <div className="reorderList">Drag and drop to reorder list</div>
+      <div className="todoList container">
+        <div className="listItems"></div>
+        <div className="todoControls">
+          <div className="control-info">
+            <p>
+              <span className="numberOfItems">5</span> items left
+            </p>
+            <button>Clear Completed</button>
+          </div>
+          <div className="items-control-group">
+            <button>All</button>
+            <button>Active</button>
+            <button>Completed</button>
+          </div>
         </div>
-
-        <footer>
-          <div className="attribution">
-            Challenge by&nbsp;
-            <a
-              href="https://www.frontendmentor.io?ref=challenge"
-              target="_blank"
-            >
-              Frontend Mentor
-            </a>
-            . Coded by <a href="#">Jolene Kearse</a>, <br />
-            <a href="https://www.ashmoreno.dev" target="_blank">
-              Ash Moreno
-            </a>
-            ,&nbsp;
-            <a href="https://wfeliciano20.github.io/Portfolio/" target="_blank">
-              William Rios
-            </a>
-            , and <a href="https://github.com/princemuel">Samuel Chukwuzube</a>.
-          </div>
-        </footer>
       </div>
+
+      <div className="reorderList container">Drag and drop to reorder list</div>
+
+
+      <footer>
+        <div className="attribution container">
+          Challenge by&nbsp;
+          <a
+            href="https://www.frontendmentor.io?ref=challenge"
+            target="_blank"
+          >
+            Frontend Mentor
+          </a>
+          . Coded by <a href="#">Jolene Kearse</a>, <br />
+          <a href="https://www.ashmoreno.dev" target="_blank">
+            Ash Moreno
+          </a>
+          ,&nbsp;
+          <a href="https://wfeliciano20.github.io/Portfolio/" target="_blank">
+            William Rios
+          </a>
+          , and <a href="https://github.com/princemuel">Samuel Chukwuzube</a>.
+        </div>
+      </footer>
     </React.Fragment>
   );
 }
