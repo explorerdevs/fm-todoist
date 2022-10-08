e2b00b3814947ad059224d1aa82d3950f79651cd
princemuel <vansomecsam@gmail.com>
2022-10-08 05:43:04 +0100

    New commit

--- client/src/components/buttons.jsx
+++ client/src/components/buttons.jsx
@@ -1,12 +1,25 @@
 import React from "react";
-import styles from "../styles/buttons.module.css";
+import { SunSvg } from "../common";
+import btnStyles from "../styles/buttons.module.css";
 
 const GradientBtn = ({ type, srtext }) => {
   return (
-    <button type={type} className={`btn ${styles["btn-checkbox"]}`}>
+    <button type={type} className={`btn ${btnStyles["btn-checkbox"]}`}>
       <span className="sr-only">{srtext}</span>
     </button>
   );
 };
 
-export { GradientBtn };
+const ThemeBtn = () => {
+  return (
+    <button
+      type="button"
+      className={`btn ${btnStyles["btn-toggle"]} ${btnStyles["checked"]}`}
+    >
+      <img src={SunSvg} alt="" />
+      <span className="sr-only">Toggle The Theme</span>
+    </button>
+  );
+};
+
+export { GradientBtn, ThemeBtn };
