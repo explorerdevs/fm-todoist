625848dad00e730ed58b8556d9f133ce502b566a
William A Feliciano Rios <31602072+wfeliciano20@users.noreply.github.com>
2022-10-14 13:29:40 -0400

    Added Functionality to form and list

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Header, TodoList } from "./components";
 
 const data = [
@@ -20,10 +20,11 @@ const data = [
 ];
 
 function App() {
+  const [refetch, setRefetch] = useState(false);
   return (
     <>
-      <Header />
-      <TodoList />
+      <Header setRefetch={setRefetch} />
+      <TodoList refetch={refetch} setRefetch={setRefetch} />
 
       <div className="container">
         <p>Drag and drop to reorder list</p>
