bbcfce6b041c091edb705524e2df00325a3e59db
princemuel <vansomecsam@gmail.com>
2022-10-18 11:12:28 +0100

    Add localstorage hook and setup theme provider

--- client/src/context/theme.jsx
+++ client/src/context/theme.jsx
@@ -1,11 +1,35 @@
-import React from "react";
-import { useState, createContext } from "react";
+import React, { createContext, useContext } from "react";
+import { useLocalStorage } from "../hooks";
 
-const ThemeContext = createContext({});
+const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState("");
+  const [theme, setTheme] = useLocalStorage("current-theme", {
+    current: "theme-light",
+  });
+
   return (
-    <ThemeContext.Provider value={{ theme }}>{children}</ThemeContext.Provider>
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </ThemeContext.Provider>
   );
+};
+
+export const useTheme = () => {
+  const context = useContext(ThemeContext);
+  if (context == undefined) {
+    throw new Error(`useTheme must be used within a ThemeProvider`);
+  }
+
+  const { theme, setTheme } = context;
+
+  const toggleTheme = () => {
+    setTheme((prev) =>
+      prev.current === "theme-light" ? `theme-dark` : `theme-light`
+    );
+  };
+
+  return {
+    toggleTheme,
+  };
 };
